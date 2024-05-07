ea2e7160dbc8744dd7c7b6cffcf5f70101a3bd6a
Luiz <lbastos722@gmail.com>
2024-05-07 10:32:29 -0300

    testes

---
+++ pages/About/script.js
@@ -0,0 +1,11 @@
+function goHomePage() { 
+  window.location.href = "/index.html"
+};
+
+function goAboutPage() { 
+  window.location.href = "/pages/About/about.html"
+}
+
+function goTechnologiesPage() { 
+  window.location = "/pages/Technologies/technologies.html"
+}
