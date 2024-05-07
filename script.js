a6cadfd6d8e9dd708040992b48f3ca318a45902a
Luiz <lbastos722@gmail.com>
2024-05-06 21:25:46 -0300

    Primeira versão para testes

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+/////////////////// Funções para ir de uma página a outra ///////////////////
+
+function goHomePage() { 
+  window.location.href = "/index.html"
+};
+
+function goAboutPage() { 
+  window.location.href = "pages/About/about.html"
+}
+
+function goTechnologiesPage() { 
+  window.location = "pages/Technologies/technologies.html"
+}
+
+function goToGitHub() {
+  window.open("https://github.com/LuizHenri16")
+}
+
+function goToInstagram() {
+  window.open("https://www.instagram.com/_._luizhenrique")
+}
+
+function goToLinkedIn() {
+  window.open("")
+}
+/////////////////// -------------------------------- ////////////////////////
