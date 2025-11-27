bccbf0d34f64faa8320049cd0df59219f166a4f1
LuizHerni16 <lbastos722@gmail.com>
2025-11-27 17:29:47 -0300

    update: skillcard ajustado

--- src/components/skillcard/skillcard.tsx
+++ src/components/skillcard/skillcard.tsx
@@ -8,7 +8,7 @@ interface SkillcardProps {
 
 export const Skillcard = ({icon, name, alt} : SkillcardProps) => {
     return (
-        <div className="h-16 flex items-center justify-center gap-2 border-custom-100 border-2 border-t-4 rounded-xl px-5 py-1 cursor-pointer bg-white">
+        <div className="h-16 flex items-center justify-center gap-2 border-custom-100 border-2 border-t-4 rounded-xl px-5 py-1 cursor-pointer bg-white hover:shadow-md">
             <img className="w-8" src={icon} alt={alt} />
             <p className="text-md font-[Roboto] text-gray-500">{name}</p>
         </div>
