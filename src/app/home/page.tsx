db879841fc26e6612fcc6d60926e8ad367da1dce
LuizHenri16 <lbastos722@gmail.com>
2026-02-12 16:23:36 -0300

    update: nada

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
             <div className='flex flex-col items-center justify-center h-screen'>
                 <Template >
                     <main className='flex flex-col justify-center items-center'>
-                        <div className='mt-5 mb-5'>  
+                        <div className='mt-5 mb-5'>
                             <img className='w-30' src="/images/foto.png" alt="" />
                         </div>
 
@@ -19,7 +19,7 @@ export default function Home() {
                         <div className="mt-10 border border-custom-100/80 w-[20%] lg:w-[50%] sm:w-[30%] md:[40%]"></div>
 
                         <div className='mt-10 mb-5'>
-                            <h2 className='text-2xl font-medium text-custom-100'>FULL STACK DEVELOPER</h2>
+                            <h2 className='text-2xl font-medium text-custom-100'>DESENVOLVEDOR FULLSTACK</h2>
                         </div>
                     </main >
                 </Template>
