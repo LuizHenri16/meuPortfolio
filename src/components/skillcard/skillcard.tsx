a0126250c095847c0cc2e30507b0154bc9f6855b
LuizHenri16 <lbastos722@gmail.com>
2026-02-12 16:21:40 -0300

    alterações no card de skills

--- src/components/skillcard/skillcard.tsx
+++ src/components/skillcard/skillcard.tsx
@@ -6,9 +6,9 @@ interface SkillcardProps {
     alt: string
 }
 
-export const Skillcard = ({icon, name, alt} : SkillcardProps) => {
+export const Skillcard = ({ icon, name, alt }: SkillcardProps) => {
     return (
-        <div className="h-16 flex items-center justify-center gap-2 border-custom-100 border-2 border-t-4 rounded-xl px-5 py-1 cursor-pointer bg-white hover:shadow-md">
+        <div className="h-16 flex items-center justify-center gap-2 border-custom-100 border-2 border-t-4 rounded-xl px-5 py-1 cursor-pointer bg-white hover:shadow-md transition-all duration-300 hover:scale-105">
             <img className="w-8" src={icon} alt={alt} />
             <p className="text-md font-[Roboto] text-gray-500">{name}</p>
         </div>
