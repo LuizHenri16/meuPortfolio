a6c16bbe70bee743cdad418925d50044f10a2e73
LuizHenri16 <lbastos722@gmail.com>
2026-03-15 21:13:29 -0300

    feature: separar seções em componentes para facilitar manutenção

---
+++ src/components/sections/footerSection.tsx
@@ -0,0 +1,9 @@
+export const FooterSection: React.FC = () => {
+    return (
+        <footer className="mt-10 md:mt-20 max-w-[1120px] mx-auto px-4">
+            <div className="flex flex-col justify-center items-center">
+                <p className="p-2 font-[Sora] text-small font-semibold text-terracota-50 opacity-80">2026 © Luiz Henrique Bastos. Todos os direitos reservados.</p>
+            </div>
+        </footer>
+    )
+}
